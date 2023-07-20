2a53c1f34cb1d19869fa1ecb82495ae89f05cad8
Andherson Costa <112807863+oAndin@users.noreply.github.com>
2023-07-20 07:58:42 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,30 @@
 console.log("Hello World!");
 function startExplaination () {
-
+    explanation.style.visibility = `visible`;
+    innerHTML = 
+    `<article>
+        <div id="fun">
+            <div id="column">
+                <label for="word">Palavra que deseja substituir</label>
+                <input id="word" type="text" placeholder="CSS">
+            </div>
+            <textarea name="" id="example" cols="30" rows="10">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus eaque reprehenderit quo dolorum praesentium quae cupiditate. Nesciunt adipisci nemo veritatis libero. Ex voluptatum aperiam nulla! Quod amet earum ducimus provident!</textarea>
+            <button>substituir</button>
+        </div>
+    </article>`;
 }
+let wordSubs;
+let example;
+let newString;
+subsBtn.onclick = function repla() {
+    wordSubs = word.value;
+    example = exampleTxt.value;
+    console.log(wordSubs);
+    console.log(example);
+    newString = example.replace(`Lorem`, `${wordSubs}`);
+    exampleTxt.innerHTML = newString;
+};
+
 darkmode.onclick = function darkMode() {
     if (normal.classList.contains(`active`)) {
         whiteMode.classList.add("active");
